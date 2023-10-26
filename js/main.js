735fa921561f085bec0efc3364b50ef573f24ae2
Isabel Robleda <isabel.robleda@gmail.com>
2023-10-26 16:22:17 +0200

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ class Player {
         this.height = 15 //the height of our player
         this.width = 20 //the width of our player
 
+
+        //dom manipulation to reflect initial values (size, position)
         this.playerElem = document.getElementById("player")
         this.playerElem.style.width = this.width + "vw"
         this.playerElem.style.height = this.height + "vh"
@@ -24,7 +26,51 @@ class Player {
 
 }
 
-const player = new Player()
+class Obstacle {
+    constructor(){
+        this.positionX = 50
+        this.positionY = 100 
+        this.height = 10
+        this.width = 15
+        this.newObstacle = undefined
+
+        this.createDomElement()
+    }
+
+    createDomElement(){
+        this.obstacleElm = document.createElement("div")
+        this.obstacleElm.classList.add("obstacle")
+        this.obstacleElm.style.width = this.width + "vw"
+        this.obstacleElm.style.height = this.height + "vh"
+        this.obstacleElm.style.left = this.positionX + "vw"
+        this.obstacleElm.style.bottom = this.positionY + "vh"
+
+        const parentElem = document.getElementById("board")
+        parentElem.appendChild(this.obstacleElm)
+    }
+    moveDown(){
+    this.positionY--
+    this.obstacleElm.style.bottom = this.positionY + "vh"
+     }
+}
+
+const player = new Player();
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+// to create obsacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+}, 2000);
+
+//move obstacles (every 50 mms move all obstavles we have in the array)
+setInterval(() => {
+    obstaclesArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown()
+    })
+}, 50)
+
+
 
 document.addEventListener("keydown", (e) => {
     switch (e.code) {
