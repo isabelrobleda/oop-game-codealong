e6f99062fa4275d97c90dfb1d42d2da611222d94
Isabel Robleda <isabel.robleda@gmail.com>
2023-10-26 12:57:18 +0200

    implement functionality to display and move the player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log("js loaded....")
+class Player {
+    constructor(){
+        this.positionX = 50// we want to place our player in an x/y axis
+        this.positionY = 0 //our player is in the bottom left corner
+        this.height = 15 //the height of our player
+        this.width = 20 //the width of our player
+
+        this.playerElem = document.getElementById("player")
+        this.playerElem.style.width = this.width + "vw"
+        this.playerElem.style.height = this.height + "vh"
+        this.playerElem.style.left = this.positionX + "vw"
+        this.playerElem.style.bottom = this.positionY + "vh"
+        
+    } 
+    moveLeft(){
+        this.positionX--
+        this.playerElem.style.left = this.positionX + "vw"
+
+    }
+    moveRight(){
+        this.positionX++
+        this.playerElem.style.left = this.positionX + "vw"
+    }
+
+}
+
+const player = new Player()
+
+document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+        case "ArrowLeft":
+            player.moveLeft()
+            break;
+        case "ArrowRight":
+            player.moveRight()
+            break;
+    }
+})
